e7a3fb9632d26b1b6ea2fdb23661e3f635d9afaf
ISOBE Kazuhiko <muramasa64@gmail.com>
2015-07-08 14:22:58 +0900

    add test fixture
    setup, cleanup security groups for test

--- test/client_test.rb
+++ test/client_test.rb
@@ -9,6 +9,7 @@ class TestClient < Test::Unit::TestCase
     # テスト群の実行前に呼ばれる
     def startup
       @@config = load_test_yaml('aws.yml')
+      @@fixture = load_test_yaml('fixture.yml')
       @@cred = aws_credentials(@@config['credentials'])
       @@ec2 = Aws::EC2::Resource.new(region: @@config['region'], credentials: @@cred)
     end
@@ -20,6 +21,7 @@ class TestClient < Test::Unit::TestCase
 
   # 毎回テスト実行前に呼ばれる
   def setup
+    TestClient::setup_security_groups(@@ec2, @@config['vpc_id'], @@fixture)
   end
 
   # テストがpassedになっている場合に、テスト実行後に呼ばれる。テスト後の状態確認とかに使える。
@@ -28,6 +30,7 @@ class TestClient < Test::Unit::TestCase
 
   # 毎回テスト実行後に呼ばれる
   def teardown
+    TestClient::cleanup_security_groups(@@ec2, @@fixture)
   end
 
   def test_show
--- test/helper.rb
+++ test/helper.rb
@@ -20,5 +20,42 @@ module Sgupdater
       end
       cred
     end
+
+    def setup_security_groups(ec2, vpc_id, fixture)
+      fixture['security_groups'].each do |param|
+        group_id = ec2.client.create_security_group(
+          {
+            group_name: param['group_name'],
+            description: param['description'],
+            vpc_id: vpc_id,
+          }
+        ).group_id
+
+        ip_permissions = []
+        param['ip_permissions'].each do |ip_perm|
+          ip_permissions << {
+            ip_protocol: ip_perm['ip_protocol'],
+            from_port: ip_perm['from_port'],
+            to_port: ip_perm['to_port'],
+            ip_ranges: ip_perm['cidr_ip'].map {|ip| {cidr_ip: ip}},
+          }
+        end
+        ec2.client.authorize_security_group_ingress(
+          {
+            group_id: group_id,
+            ip_permissions: ip_permissions
+          }
+        )
+      end
+    end
+
+    def cleanup_security_groups(ec2, fixture)
+      group_names = fixture['security_groups'].map {|sg| sg['group_name']}
+      ec2.client.describe_security_groups(
+        filters: [{name: 'group-name', values: group_names}]
+      ).security_groups.each do |sg|
+        ec2.client.delete_security_group(group_id: sg.group_id)
+      end
+    end
   end
 end
